db898b1a3385fd0472ba8c782703cae801302ec1
Beraldi777 <cauaberaldiiphone012@gmail.com>
2025-04-17 11:10:33 -0300

    Bug Fixing
    - Problem with negative numbers in the step and at the beginning and end solved. Counting system changed, being able to count up to 114, so change that.

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,19 +5,21 @@ var pas = document.getElementById("n3txt");
 var res = document.getElementById("restxt");
 if (inc.value.length == 0 || fim.value.length == 0 || pas.value.length == 0) {
     alert("[ERRO] Faltam Dados!")
-    } else if (inc.value >= 115 || fim.value >= 115) {
-        alert("O Valor não pode ultrapassar 114 números!!")
+    } else if (inc.value >= 115 || fim.value >= 115 || inc.value <= -115 || fim.value <= -115) {
+        alert("[ERRO] O Valor não pode ultrapassar 114 números!!")
+    } else if (pas.value <= -1) {
+        alert("[ERRO] O passo não pode ser negativo")
     } else {
         res.innerHTML = " Contando:"
         var i = Number(inc.value);
         var f = Number(fim.value);
         var p = Number(pas.value);
         if (p == 0) {
-            alert("Passo Inválido! Considerando Passo 1.");
+            alert("[ERRO] Passo Inválido! Considerando Passo 1.");
             p = 1
-        } else if (i >= 36 || f >= 36 && p == 1) {
+        } else if (i >= 36 || f >= 36 || i <= -36 || f <= -36 && p == 1) {
             res.style.fontSize = "12pt";
-        } else if (i >= 111 || f >= 111 && p == 1) {
+        } else if (i >= 111 || f >= 111 || i <= -111 || f <= -111 && p == 1) {
             res.style.fontSize = "8pt";
         }
         if (i < f) {
